5f0a0d7b48b0ab10587d92004a25c9172480b5fc
thodinh <thodinh@innoria.com>
2022-03-21 16:38:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-window.addEventListener('load', function() {
-  let page = location.hash.replace('#');
-  if (!page) page = "README";
-  loadPage(page);
-});
+// window.addEventListener('load', function() {
+//   let page = location.hash.replace('#');
+//   if (!page) page = "README";
+//   loadPage(page);
+// });
 
-async function loadPage(page) {
-  let markdown = await (await fetch("https://raw.githubusercontent.com/meteorsphere/meteorsphere.github.io/main/" + page + ".md")).text();
-  let converter = new showdown.Converter({metadata: true});
-  let html = converter.makeHtml(markdown);
-  document.body.innerHTML = html;
-}
+// async function loadPage(page) {
+//   let markdown = await (await fetch("https://raw.githubusercontent.com/meteorsphere/meteorsphere.github.io/main/" + page + ".md")).text();
+//   let converter = new showdown.Converter({metadata: true});
+//   let html = converter.makeHtml(markdown);
+//   document.body.innerHTML = html;
+// }
+window.location = "https://meteorsphere.github.io/pages/index";
