9e27dd9006c102ce993f01c508b12cd103520c27
thodinh <thodinh@innoria.com>
2022-03-21 16:16:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 window.addEventListener('load', function() {
-  console.log('LOAD');
+  let page = location.hash.replace('#');
+  if (!page) page = "README";
+  loadPage(page);
 });
+
+async function loadPage(page) {
+  let markdown = await (await fetch("https://raw.githubusercontent.com/meteorsphere/meteorsphere.github.io/main/" + page + ".md")).text();
+  let converter = new showdown.Converter({metadata: true});
+  let html = converter.makeHtml(markdown);
+  document.body.innerHTML = html;
+}
